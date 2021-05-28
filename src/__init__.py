d7bff9963c425285f9fcaa3906fa5f9e16b1c888
Nate Brooks <nateonguitar@gmail.com>
2021-05-28 12:05:41 -0600

    detect matches

--- src/__init__.py
+++ src/__init__.py
@@ -36,15 +36,55 @@ class Match3Game(Game):
             key_character = key.char
         except:
             pass
-        if key_character == 'w' or key == keyboard.Key.up:
+        if key_character is 'w' or key == keyboard.Key.up:
             self.cursor.move('up')
             return
-        if key_character == 's' or key == keyboard.Key.down:
+        if key_character is 's' or key == keyboard.Key.down:
             self.cursor.move('down')
             return
-        if key_character == 'a' or key == keyboard.Key.left:
+        if key_character is 'a' or key == keyboard.Key.left:
             self.cursor.move('left')
             return
-        if key_character == 'd' or key == keyboard.Key.right:
+        if key_character is 'd' or key == keyboard.Key.right:
             self.cursor.move('right')
             return
+
+        if key == keyboard.Key.space:
+            self.end_turn()
+            return
+
+    def end_turn(self):
+        m = self.board.matrix
+        match_sizes = []
+        for i in range(len(m)):
+            for j in range(len(m[i])):
+                char = m[i][j]
+                if char is None:
+                    continue
+                down = 1
+                right = 1
+                # down
+                for k in range(i+1, len(m)):
+                    if m[k][j] is char:
+                        down += 1
+                    else:
+                        break
+                # right
+                for k in range(j+1, len(m[i])):
+                    if m[i][k] is char:
+                        right += 1
+                    else:
+                        break
+                scoring = down >= 3 or right >= 3
+                if scoring:
+                    if down > right:
+                        match_sizes.append(down)
+                        for k in range(i, i+down):
+                            if m[k][j] is char:
+                                m[k][j] = None
+                    else:
+                        match_sizes.append(right)
+                        for k in range(j, j+right):
+                            if m[i][k] is char:
+                                m[i][k] = None
+        self.board.fill()
