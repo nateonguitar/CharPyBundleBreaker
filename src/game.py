2c8c055952e634e07bdaec68f7180a108556bf33
Nate Brooks <nateonguitar@gmail.com>
2021-05-30 17:38:24 -0600

    renamed to match class

--- src/game.py
+++ src/game.py
@@ -18,21 +18,21 @@ class Match3Game(Game):
         self.set_on_keydown(self.on_key_down)
         self.set_on_keyup(self.on_key_up)
         self.start_screen = StartScreen()
-        self.board = None
+        self.game_board = None
 
 
     def update(self, deltatime: datetime.timedelta):
         if self.start_screen is not None:
             self.start_screen.update(deltatime)
-        if self.board is not None:
-            self.board.update(deltatime)
+        if self.game_board is not None:
+            self.game_board.update(deltatime)
 
 
     def draw(self):
         if self.start_screen is not None:
             self.start_screen.draw(self.screen)
-        if self.board is not None:
-            self.board.draw(self.screen)
+        if self.game_board is not None:
+            self.game_board.draw(self.screen)
         super().draw()
 
 
@@ -42,17 +42,17 @@ class Match3Game(Game):
             return
         if self.start_screen is not None:
             self.start_screen.on_key_down(key)
-        if self.board is not None:
-            self.board.on_key_down(key)
+        if self.game_board is not None:
+            self.game_board.on_key_down(key)
 
 
     def on_key_up(self, key: keyboard.Key):
         if self.start_screen is not None:
             self.start_screen.on_key_up(key)
-        if self.board is not None:
-            self.board.on_key_up(key)
+        if self.game_board is not None:
+            self.game_board.on_key_up(key)
 
 
     def start_game(self):
         self.start_screen = None
-        self.board = GameBoard()
+        self.game_board = GameBoard()
